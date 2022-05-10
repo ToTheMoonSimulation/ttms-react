7e4bb34f402476a73522bb9c24367548453bbba0
RamRam-V <cajhyung@gmail.com>
2022-05-10 11:28:04 +0900

    added proxy, axios

--- src/layout/main/components/contents.js
+++ src/layout/main/components/contents.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Grid from "@mui/material/Grid";
-import Button from '@mui/material/Button';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import Button from "@mui/material/Button";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 //import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
 
 import waveSvg from "../../../assets/wave.svg";
 const Contents = () => {
+    const onClickPlay = () => {
+        console.log("A");
+        axios
+            .get("/api/users/test")
+            .then((Response) => {
+                console.log(Response.data);
+            })
+            .catch((Error) => {
+                console.log(Error);
+            });
+    };
+
+    useEffect( () => {
+        console.log("useEffect");
+    },[]);
     return (
         <>
             {/* 서비스 소개 */}
@@ -20,12 +36,16 @@ const Contents = () => {
                     height: "500px",
                 }}
             >
-                <Grid item sx={{ color: "white", textAlign:"center"}}>
+                <Grid item sx={{ color: "white", textAlign: "center" }}>
                     <h1>Investment Game For Crypto Currency💸</h1>
                     <h1>Try Free</h1>
                 </Grid>
                 <Grid item>
-                    <Button variant="contained" endIcon={<PlayArrowIcon />}>
+                    <Button
+                        variant="contained"
+                        endIcon={<PlayArrowIcon />}
+                        onClick={onClickPlay}
+                    >
                         Play
                     </Button>
                 </Grid>
